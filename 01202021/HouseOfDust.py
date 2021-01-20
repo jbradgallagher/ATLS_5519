aaa1556c92bf008fc4efd7fce36f424e49ef3a43
Brad Gallagher <jbradgallagher@gmail.com>
2021-01-19 20:25:46 -0700

    Module #1, House of Dust, permutation poems, import stub

--- 01202021/HouseOfDust.py
+++ 01202021/HouseOfDust.py
@@ -12,39 +12,14 @@ lineThreeBegin = "USING "
 lineFourBegin = "INHABITED BY "
 
 def getRandomWord(wrdList):
-	return wrdList[random.randint(0,len(wrdList)-1)]
 
 def makeSomeSpace(num):
-	return ''.join(' ' for i in range(num)) 
 	
 def buildPoem():
-	poem = []
-	
-	poem.append(lineOneBegin+getRandomWord(materials)+makeSomeSpace(12)+lineFourBegin+getRandomWord(inhabit))
-	poem.append(makeSomeSpace(4)+getRandomWord(preps)+makeSomeSpace(1)+getRandomWord(places)+makeSomeSpace(8)+lineThreeBegin+getRandomWord(lights))
-	poem.append(makeSomeSpace(8)+lineThreeBegin+getRandomWord(lights)+makeSomeSpace(4)+getRandomWord(preps)+makeSomeSpace(1)+getRandomWord(places))
-	poem.append(makeSomeSpace(12)+lineFourBegin+getRandomWord(inhabit)+lineOneBegin+getRandomWord(materials))
-
-	return poem
 
 def printPoem():
-	print()
-	for line in buildPoem():
-		print(line)		
-	print()
-
-def printPoemByLetter():
-	for line in buildPoem():
-		for wrd in line:
-			print(wrd, end='', flush="True")
-			time.sleep(1/20.)
-		print()
-			#for char in wrd:
-			#	print(char, end="", flush="True")
-			#	time.sleep(1/20.)
 
 def main():
-	printPoemByLetter()
 
 if __name__ == '__main__':
 	main()
