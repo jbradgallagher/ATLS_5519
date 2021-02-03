31695bbe8f582b3009683ce6d2d1eabc0157a964
Brad Gallagher <jbradgallagher@gmail.com>
2021-02-03 10:33:47 -0700

    initial commit for hyperlink remix exercise for class on 02032021

---
+++ 02032021/hyperlink_remix.py
@@ -0,0 +1,157 @@
+import io
+import os
+import sys
+import random
+import re
+import getopt
+import glob
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download('stopwords')
+nltk.download('punkt')
+stop_words = set(stopwords.words('english'))
+
+
+def removeDups(myList):
+	newList = []
+	for i in myList:
+		if i not in newList:
+			newList.append(i)
+	return newList
+
+def returnWords(line):
+	new_words = []
+	words = nltk.word_tokenize(line)
+	for wrd in [word.lower() for word in words if (word.isalnum() or re.search(r'\-',word))]:
+		if not wrd in stop_words and len(wrd) > 5:
+			new_words.append(wrd)
+			
+	return new_words
+
+
+def getFileDict(prefixName):
+	fileDict = {}
+	fileList = glob.glob(prefixName+"*")
+
+	for file in fileList:
+		fileHandle = open(file.strip('\n'), "r")
+		fileContents = fileHandle.read();
+		fileDict[file.strip('\n').split('.')[0]] = returnWords(fileContents)
+
+	return fileDict
+
+
+
+def hypertextRemix(corpusOnePrefix,corpusTwoPrefix,whichFrame):
+
+	wordsToLink = []
+	corpusOneDict = getFileDict(corpusOnePrefix)
+	corpusTwoDict = getFileDict(corpusTwoPrefix)
+	linkToFileDict = dict() 
+	for fileNameOneKey in corpusOneDict:
+
+		allFoundWords = []
+		filesToLink = []
+		for fileNameTwoKey in corpusTwoDict:
+			#go over the list of filtered words for corpusTwo file "pkey"
+			for wrd in corpusTwoDict[fileNameTwoKey]:
+				if wrd in corpusOneDict[fileNameOneKey]:
+					allFoundWords.append(wrd)
+					print("H: ",fileNameOneKey,fileNameTwoKey,wrd)
+					try:
+						linkToFileDict[wrd].append(fileNameTwoKey)
+					except:
+						linkToFileDict[wrd] = []
+						linkToFileDict[wrd].append(fileNameTwoKey)
+					#filesToLink.append(fileNameTwoKey)
+					
+					
+		wordsToLink = removeDups(allFoundWords)
+
+		#now we write the HTML file for the first file in corpusOneDict
+		outHTMLFname = fileNameOneKey + ".html"
+		inFileName = fileNameOneKey + ".txt"
+		inTextFile = open(inFileName, "r")
+		outHTMLFile = open(outHTMLFname, "w")
+
+		inTextLines = inTextFile.readlines()
+	
+		outHTMLFile.write("<html>\n<head>\n")
+		outHTMLFile.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\n")
+		outHTMLFile.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"hylrm.css\" />\n")
+		outHTMLFile.write("</head>\n")
+		outHTMLFile.write("<body>\n")
+		outHTMLFile.write("<div id=\"poem\">\n")
+
+		for line in inTextLines:
+			newline = []
+			myLine = ""
+				
+			wrds = line.split()
+			for wrd in wrds:
+				foundWrd = False
+				saveWrd = ""
+				for lwrd in wordsToLink:
+					if re.search(lwrd, wrd.lower()):
+						foundWrd = True
+						saveWrd = lwrd
+						break
+				if foundWrd:
+					foundWrd = False
+					fileToLink = linkToFileDict[saveWrd.lower()][random.randint(0,len(linkToFileDict[saveWrd.lower()])-1)] + ".html"
+					link = "<a href=\"" + fileToLink + "\"" + " target=\"" + whichFrame + "\"" + " style=\"color:000000;\">" + wrd + "</a>"
+					newline.append(link)
+				else:
+					newline.append(wrd)
+
+			newline.append("</br>")
+			myLine = " ".join(newline)
+			myLine = myLine + "\n"
+			outHTMLFile.write(myLine)
+
+		inTextFile.close()
+		outHTMLFile.write("</p>\n</div>\n</body>\n</html\n>")
+		outHTMLFile.close()
+
+def printUsage():
+	print()
+	print("Usage: " + sys.argv[0] + " -p <corpusPrefixOne> -q <corpusPrefixTwo> -f (left || right)")
+	print()
+
+def main():
+	corpPrefixOne = ""
+	corpPrefixTwo = ""
+	makeFrame = ""
+
+	if(len(sys.argv) == 1):
+			printUsage()
+	else:
+		try:
+			opts, args = getopt.getopt(sys.argv[1:], 'p:q:f:')
+			for o, a in opts:
+				if o == '-p':
+					corpPrefixOne = a
+				if o == '-q':
+					corpPrefixTwo = a
+				if o == '-f':
+					makeFrame = a
+
+			hypertextRemix(corpPrefixOne,corpPrefixTwo,makeFrame)
+
+		except getopt.GetoptError as err:
+			print(err)
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+	
+
+
+	
